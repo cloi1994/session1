0efa53e1871d0e7127fc759bb0ca95516e1ac48a
cloi1994 <31608472+cloi1994@users.noreply.github.com>
2018-04-08 12:55:07 -0700

    Create 22.py

---
+++ Uber/22.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        tmp = ""
+        res = []
+        self.dfs(0,0,n,tmp,res)
+        
+        return res
+        
+    def dfs(self,left,right,n,tmp,res):
+        
+        if len(tmp) == 2*n:
+            res.append(tmp)
+            return
+        
+        if left < n:
+            self.dfs(left+1,right,n,tmp+'(',res)
+        
+        if left > right:
+            self.dfs(left,right+1,n,tmp+')',res)
+        
