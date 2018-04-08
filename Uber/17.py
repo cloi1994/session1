17fa14104de95e63f0c07116e77b5c9d071949f6
cloi1994 <31608472+cloi1994@users.noreply.github.com>
2018-04-08 12:06:48 -0700

    Create 17.py

---
+++ Uber/17.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        
+        hm = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        
+        tmp = ""
+        res = []
+        self.dfs(0,digits,tmp,res,hm)
+        return res
+        
+    def dfs(self,level,digits,tmp,res,hm):
+        
+        if len(tmp) == len(digits):
+            res.append(tmp)
+            return
+        
+        for c in hm[digits[level]]:
+            self.dfs(level+1,digits,tmp+c,res,hm)
+        
