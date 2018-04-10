482b9110a582fa0a5274b782607688141440cdc9
cloi1994 <31608472+cloi1994@users.noreply.github.com>
2018-04-09 23:09:42 -0700

    Create 535.py

---
+++ Uber/535.py
@@ -0,0 +1,29 @@
+/*
+It produces short URLs like http://tinyurl.com/KtLa2U, using a random code of six digits or letters. 
+If a long URL is already known, the existing short URL is used and no new entry is generated.
+/*
+
+
+class Codec:
+    
+    alphabet = string.ascii_letters + '0123456789'
+
+    def __init__(self):
+        self.url2code = {}
+        self.code2url = {}
+
+    def encode(self, longUrl):
+        if longUrl not in self.url2code:
+            code = ''.join(random.choice(Codec.alphabet) for _ in range(6))
+            if code not in self.code2url:
+                self.code2url[code] = longUrl
+                self.url2code[longUrl] = code
+        return 'http://tinyurl.com/' + self.url2code[longUrl]
+
+    def decode(self, shortUrl):
+        return self.code2url[shortUrl[-6:]]
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
