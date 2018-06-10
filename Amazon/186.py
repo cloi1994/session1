ad0620574c2ecb3b010d913fc608e4ffe1ba6b01
cloi1994 <31608472+cloi1994@users.noreply.github.com>
2018-06-10 14:37:12 -0700

    Create 186.py

---
+++ Amazon/186.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+        if not s:
+            return " "
+        
+        left = 0
+        
+        s = list(s)
+        
+        for i in range(len(s)+1):
+            if i == len(s) or s[i] == ' ':
+                self.reverse(s,left,i-1)
+                left = i + 1
+                
+        self.reverse(s,0,len(s)-1)
+        
+        return ''.join(s)
+        
+    def reverse(self,s,left,right):
+                
+        while left < right:
+        
+            s[left] , s[right] = s[right] , s[left]
+            left +=1
+            right -= 1
+        
+        
+        
+        
